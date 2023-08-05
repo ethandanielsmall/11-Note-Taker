c20dd255423bedf7ee4dba8e9d564cc3f71ebd37
Ethan Daniel <e.daniel.small@gmail.com>
2023-08-04 19:09:12 -0700

    functional when ran with node server.js

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 
 // Specific routes
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
 // Static files middleware
